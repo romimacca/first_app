9521249aa110171e41c75f37a71591aef16c5a10
Romi Maccarini <romi@clay.cl>
2020-07-16 14:01:42 -0400

    set homepage, add rake routes image

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  root 'pages#one'
+
+  get 'pages/one'
+  get 'pages/two'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
